0b7a8787087530d377098d97212c74408a2b5dbe
Masoko <jeleff@gmail.com>
2021-12-04 12:36:58 +0000

    update

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 echo starting pelican
 cd /home/pi/test/funmasokonet.github.io
+git checkout main
 /home/pi/sites/pelican/bin/pelican content
 echo done with pelican
 now=$(date)
@@ -10,4 +11,5 @@ git add .
 git commit -m "$msg"
 git push https://funmasokonet:$FUNGITTOKEN@github.com/funmasokonet/funmasokonet.github.io.git origin gh-pages
 cd ..
+git checkout main
 echo done
